b6b5adc9e902988d29a207c86087a918336beca3
Fédi <fedielmanoubi@gmail.com>
2023-03-08 20:35:54 +0100

    PremierCommit

---
+++ vues/footer.php
@@ -0,0 +1,41 @@
+ </div>
+        <div id="modalSupp"class="modal fade" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Confirmation de Suppression</h5>
+          </div>
+          <div class="modal-body">
+            <p>êtes-vous sûr de vouloir supprimer cette nationalité ?</p>
+          </div>
+          <div class="modal-footer">
+            <a href="="class="btn btn-primary" id="btnSuppr">Supprimer</a>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Ne pas Supprimer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="container">
+      <p>&copy; Company 2017-2018</p>
+    </footer>
+        
+     
+      
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+  <script> type="text/javascript">
+
+$("a[data-supp]").click(function(){
+   var lien = $(this).attr("data-supp");
+   var message = $(this).attr("data-message");
+   $("#btnSuppr").attr("href",lien);
+   $(".modal-body").text(message);
+});
+
+  </script>
+</body>
+</html>
---
+++ vues/header.php
@@ -0,0 +1,60 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.css">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+      <a class="navbar-brand" href="#">Ma Bibliothèque</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+         <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="listeNationalite.php" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gestion des genres</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Liste des genres</a>
+              <a class="dropdown-item" href="#">ajouter un genres</a>
+            </div>
+            </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gestion des auteurs</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Liste des auteurs</a>
+              <a class="dropdown-item" href="#">ajouter un auteurs</a>
+              <a class="dropdown-item" href="#">Rechercher un auteurs</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="listeNationalite.php" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gestion des Nationalités</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="listeNationalite.php">Liste des Nationalités</a>
+              <a class="dropdown-item" href="formAjoutNationalite.php">ajouter un Nationalités</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
